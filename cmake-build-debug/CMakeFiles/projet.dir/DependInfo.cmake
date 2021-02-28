2446a0b8cf9459eda0ba088c29f2543c736d4e73
camillecaulier <camillecaulier.cc@gmail.com>
2021-02-28 18:22:31 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/projet.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/camille626/Documents/bac_3/linfo1341_reseaux/projet/src/log.c" "D:/camille626/Documents/bac_3/linfo1341_reseaux/projet/cmake-build-debug/CMakeFiles/projet.dir/src/log.c.obj"
+  "D:/camille626/Documents/bac_3/linfo1341_reseaux/projet/src/receiver.c" "D:/camille626/Documents/bac_3/linfo1341_reseaux/projet/cmake-build-debug/CMakeFiles/projet.dir/src/receiver.c.obj"
+  "D:/camille626/Documents/bac_3/linfo1341_reseaux/projet/src/sender.c" "D:/camille626/Documents/bac_3/linfo1341_reseaux/projet/cmake-build-debug/CMakeFiles/projet.dir/src/sender.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
