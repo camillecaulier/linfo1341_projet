a633e8c5b002018ccfeeb64d3679abcb42ceabc8
camillecaulier <camillecaulier.cc@gmail.com>
2021-03-28 12:02:59 +0200

    valgrind test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 # Note that this assumes to be called from the Makefile, you may want to adapt it.
 echo "A very simple test"
-./tests/test.sh
+./tests/simple_test_val.sh
 # Run the same test, but this time with valgrind #VALGRIND=1 ./tests/simple_test.sh
 echo "A very simple test, with Valgrind"
 
